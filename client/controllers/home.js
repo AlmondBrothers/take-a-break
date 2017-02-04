1dc4bcdac2548f165e3972f6dad162361bf602a6
svey <marcussvehlak.code@gmail.com>
2017-02-03 19:43:59 -0800

    Rendering database break items to client home view.

--- client/controllers/home.js
+++ client/controllers/home.js
@@ -1,11 +1,11 @@
 myApp.controller('home', function($scope, aBreak) {
-  // var $scope.Break = {};
+  $scope.break = {};
   // console.log(aBreak.getBreak());
   var getABreak = function() {
     aBreak.getBreak().then(function(someBreak) {
       console.log('Home.js - Get a Break: ', someBreak);
-      // $scope.title = someBreak.title;
-      // $scope.description = someBreak.description;
+        $scope.break.title = someBreak[0].title;
+        $scope.break.description = someBreak[0].description;
     });
   };
   getABreak();
@@ -15,7 +15,7 @@ myApp.controller('home', function($scope, aBreak) {
   //   // $scope.title = someBreak.title;
   //   // $scope.description = someBreak.description;
   // });
-  console.log('Home.js - Got the specificBreak: ', specificBreak);
+  //console.log('Home.js - Got the specificBreak: ', specificBreak);
   // $scope.title = specificBreak.title;
   // $scope.description = specificBreak.description;
 });
