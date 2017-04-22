6019cdf1b641aa8de672de0f7922f0ab05b2e2ca
svey <marcus.svehlak@gmail.com>
2017-04-21 22:48:35 -0700

    Refactored server and passport implementations to es6. Made changes to HTML took out excessive DOM elements.

--- app/routes.js
+++ app/routes.js
@@ -1,50 +1,37 @@
-// app/routes.js
-
-module.exports = function(app, passport){
-    // route for home page
-    app.get('/', function(req, res) {
-        res.render('index.html'); // load the index.ejs file
+module.exports = (app, passport) => {
+  // route for home page
+  app.get('/', (req, res) => {
+    res.render('index.html'); // load the index.ejs file
+  });
+  // route for showing the profile page
+  app.get('/profile', isLoggedIn, (req, res) => {
+    res.render('profile.ejs', {
+      user: req.user // get the user out of session and pass to template
     });
-
-    // route for login form
-    // route for processing the login form
-    // route for signup form
-    // route for processing the signup form
-
-    // route for showing the profile page
-    app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
-            user : req.user // get the user out of session and pass to template
-        });
-    });
-
-    // route for logging out
-    app.get('/logout', function(req, res) {
-        req.logout();
-        res.redirect('/home');
-    });
-
-    // send to google to do the authentication
-    // profile gets us their basic information including their name
-    // email gets their emails
-    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
-
-    // the callback after google has authenticated the user
-    app.get('/auth/google/callback',
-            passport.authenticate('google', {
-                    successRedirect : '/',
-                    failureRedirect : '/'
-            }));
-
+  });
+  // route for logging out
+  app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/home');
+  });
+  // send to google to do the authentication
+  // profile gets us their basic information including their name
+  // email gets their emails
+  app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
+  // the callback after google has authenticated the user
+  app.get('/auth/google/callback',
+    passport.authenticate('google', {
+      successRedirect: '/',
+      failureRedirect: '/'
+  }));
 };
 
 // route middleware to make sure a user is logged in
-function isLoggedIn(req, res, next) {
-
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
-
-    // if they aren't redirect them to the home page
-    res.redirect('/');
-}
+const isLoggedIn = (req, res, next) => {
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated()) return next();
+  // if they aren't redirect them to the home page
+  res.redirect('/');
+};
