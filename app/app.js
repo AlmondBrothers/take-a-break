b1f8b03e52999a87269efe90b1e3e28458266e78
svey <marcus.svehlak@gmail.com>
2017-04-24 15:04:38 -0700

    --allow-empty

--- app/app.js
+++ app/app.js
@@ -13,5 +13,5 @@ app.config(function ($routeProvider, $locationProvider) {
   .otherwise({
     redirectTo: '/',
   });
-  $locationProvider.hashPrefix('');
+  $locationProvider.html5Mode(true);
 });
