e503113cd6e4a8c01cd877ecfc27df989b53c431
svey <marcus.svehlak@gmail.com>
2017-04-24 14:55:29 -0700

    --allow-empty

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
   googleAuth: {
     clientID: '175873763399-4q5p8tdbhqttt1rcr8bsurdpgcfinmee.apps.googleusercontent.com',
     clientSecret: 'ObTpx5xFR7xq_Qtb-o31K-oc',
-    callbackURL: 'http://upbreaks.com/auth/google/callback'
+    callbackURL: 'http://www.upbreaks.com/auth/google/callback'
   }
 };
